b0efe4f1f230e676268c63421ed6f251222b2258
William Cory <williamcory@Williams-Mac-mini.local>
2026-01-18 14:50:03 -0800

    test commit

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,6 @@
 // Smithers hooks
 export { useRalphCount } from './useRalphCount.js'
 export * from './useHuman.js'
+export * from './useHumanInteractive.js'
 export { useCaptureRenderFrame } from './useCaptureRenderFrame.js'
 export { useCommitWithRetry } from './useCommitWithRetry.js'
